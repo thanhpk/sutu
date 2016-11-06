43f4f6c85c602c614a5176209a44e3e26360d6a4
Thanh Pham <thanhpk@live.com>
2016-11-06 20:01:56 +0700

    setup server and test

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var supertest = require("supertest");
+var should = require("should");
+
+var server = supertest.agent("http://localhost:4000");
+
+describe("Sample unit test", function() {
+	it("should return home page", function(done) {
+		server.get("/")
+			.expect("Content-type", /text/)
+			.expect(200)
+			.end(function(err, res) {
+				res.status.should.equal(200);
+				done();
+			});
+	});
+		
+});
